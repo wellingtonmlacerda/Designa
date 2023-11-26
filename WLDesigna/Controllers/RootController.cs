a1012131fb02a8e0e4c1df117f07302a27631358
wellingtonmlacerda <wellingtonmlacerda@gmail.com>
2023-11-26 19:27:22 -0300

    Adcionado controller root

---
+++ WLDesigna/Controllers/RootController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WLDesigna.Controllers
+{
+    public class RootController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
